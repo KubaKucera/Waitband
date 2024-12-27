05ca042e5c00f5c4a32ddc47967c4cc79efdc3a4
kucera.jakub <kucera.jakub@ssakhk.cz>
2024-12-27 21:49:45 +0100

    Add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,21 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Head from "next/head";
-
-/*
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});*/
 
 export const metadata: Metadata = {
-  title: "Wait - Oficiální web české crossover kapely",
+  title: "Wait - Oficiální Web České Crossover Kapely",
   description: "Wait Official page by create next app",
   icons: {
-    icon:['/favicon.ico?v=4'],
-    apple:['/apple-touch-icon.png?v=4'],
-    shortcut:['/apple-touch-icon.png']
-  }
-}
+    icon: [
+      { url: "/favicon.ico?v=4" },
+      { url: "/icon.svg", type: "image/svg+xml" },
+    ],
+    apple: [
+      { url: "/apple-touch-icon.png?v=4", sizes: "180x180" },
+    ],
+    shortcut: [{ url: "/apple-touch-icon.png" }],
+  },
+};
 
 export default function RootLayout({
   children,
@@ -32,18 +24,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        //className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Head>
-          <link rel="icon" href="/favicon.ico?v=4" />
-          <link rel="shortcut icon" href="/favicon.ico?v=4" />
-          <link rel="apple-touch-icon" href="/apple-touch-icon.png?v=4" />
-          <meta name="theme-color" content="#ffffff" />
-        </Head>
-
+      <body>
         {children}
-
       </body>
     </html>
   );
