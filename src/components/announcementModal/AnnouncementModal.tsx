e3a26067ad2a8131a32180668e4386b0eb112625
kucera.jakub <kucera.jakub@ssakhk.cz>
2025-11-09 17:06:21 +0100

    Add

--- src/components/announcementModal/AnnouncementModal.tsx
+++ src/components/announcementModal/AnnouncementModal.tsx
@@ -45,7 +45,7 @@ export default function AnnouncementModal() {
             <motion.div
                 className="relative bg-gradient-to-b from-gray-50 via-gray-100 to-gray-200 text-white rounded-2xl shadow-2xl max-w-lg monitor:max-w-2xl w-full mx-8 overflow-hidden"
                 initial={{ y: -40, opacity: 0 }}                
-                animate={{ y: 0, scale: isLargeScreen ? 1.15 : 1, opacity: 1 }}
+                animate={{ y: 0, scale: isLargeScreen ? 1.1 : 1, opacity: 1 }}
                 exit={{ y: 20, opacity: 0 }}
                 transition={{ type: "spring", stiffness: 120, damping: 20 }}
                 onClick={(e) => e.stopPropagation()}
